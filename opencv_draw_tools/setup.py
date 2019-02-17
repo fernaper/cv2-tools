15e0120aabf0917d22e11ae30d4028bc359f05e2
fernaperg <fernaperg@gmail.com>
2019-02-17 21:20:50 +0100

    :sparkles: #2 Preparing setup.py

---
+++ opencv_draw_tools/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="opencv-draw-tools-fernaperg",
+    version="0.0.4",
+    author="Fernando Pérez",
+    author_email="fernaperg@gmail.com",
+    description="Library to help the drawing process with OpenCV. Thought to add labels to the images. Classification of images, etc.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fernaper/opencv-draw-tools",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'opencv-python',
+        'numpy'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
