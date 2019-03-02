a4fe727cf314f5406b1a01f099139ecf29c20aa0
fernaperg <fernaperg@gmail.com>
2019-03-02 20:15:58 +0100

    :sparkles: Now tags supports multiline text :sparkles: Now select_zone supports filled rectangles

--- opencv_draw_tools/__init__.py
+++ opencv_draw_tools/__init__.py
@@ -6,7 +6,7 @@ MIT License
 Copyright (c) 2019 Fernando Perez
 For more information visit: https://github.com/fernaper/opencv-draw-tools
 Also you can write complete_help to view full information'''
-__version__ =  '0.1.8'
+__version__ =  '0.1.9'
 
 complete_help = '''
 {} - v{}
