e91a390a21f21689f16133eee4836db66a46e551
fernaperg <fernaperg@gmail.com>
2019-03-10 16:29:18 +0100

    :tada: v1.0.0 #21 #22 Now you can select multiple zones at the same time :sparkles: It is really smart selecting where to place tags :bug: Also fixed bugs with tags placement :bug: Detected bug when changing the margin parameter (for now, dont change it)

--- opencv_draw_tools/tags_constraint.py
+++ opencv_draw_tools/tags_constraint.py
@@ -1,5 +1,5 @@
 from constraint import *
-import pprint
+import cv2
 
 def position_inside_rectangle(position, rectangle):
     return position[0] > rectangle[0] and position[0] < rectangle[2] and \
@@ -17,18 +17,22 @@ def rectangle_collision_list_rectangles (rectangle, list_rectangles):
             return True
     return False
 
-def get_possible_positions(width, height, all_selected_zones, all_tags_shapes):
+def get_possible_positions(width, height, all_selected_zones, all_tags_shapes, margin=5, frame=[]):
     problem = Problem()
     for i, rectangle in enumerate(all_selected_zones):
         tag_width, tag_height = all_tags_shapes[i]
-        problem.addVariable('zone_{}'.format(i), [(rectangle[2], rectangle[3] - tag_height, rectangle[2] + tag_width, rectangle[3], 'bottom_right'),
-                                                  (rectangle[0] - tag_width, rectangle[3] - tag_height, rectangle[0], rectangle[3], 'bottom_left'),
-                                                  (rectangle[0], rectangle[1], rectangle[0]+tag_width, rectangle[1]+tag_height, 'inside'),
-                                                  (rectangle[0], rectangle[1] - tag_height, rectangle[0] + tag_width, rectangle[1], 'top')])
-        # Only accepts zones inside the frame
-        problem.addConstraint(lambda zone: rectangle_collision_rectangle(zone[:-1], (0,0,width, height)), ('zone_{}'.format(i),))
-        # Only accpets tags not inside other selections
-        problem.addConstraint(lambda zone: not rectangle_collision_list_rectangles(zone[:-1], all_selected_zones), ('zone_{}'.format(i),))
+        sides = [(rectangle[2], rectangle[3] - tag_height - margin, rectangle[2] + tag_width, rectangle[3] - margin*2, 'bottom_right'),
+                (rectangle[0] - tag_width, rectangle[3] - tag_height - margin, rectangle[0], rectangle[3] - margin*2, 'bottom_left'),
+                (rectangle[0], rectangle[1], rectangle[0] + tag_width, rectangle[1] + tag_height + margin*2, 'inside'),
+                (rectangle[0] + margin, rectangle[1] - tag_height - margin, rectangle[0] + tag_width, rectangle[1], 'top')]
+        valid_sides = []
+        other_selected_zones = all_selected_zones[:i] + all_selected_zones[i+1:]
+        for side in sides:
+            # Only accepts zones inside the frame with tags not inside other selections
+            if rectangle_collision_rectangle(side[:-1], (0, 0, width, height)) and \
+                not rectangle_collision_list_rectangles(side[:-1], other_selected_zones):
+                valid_sides.append(side)
+        problem.addVariable('zone_{}'.format(i), valid_sides)
 
     for i in range(len(all_selected_zones) - 1):
         # Only accepts tags not collisioning with other tags
@@ -43,6 +47,7 @@ def get_possible_positions(width, height, all_selected_zones, all_tags_shapes):
         current_score = 0
         for key in solution.keys():
             position = solution[key][4]
+            info_zone = solution[key]
             if position == 'bottom_right':
                 current_score += 4
             elif position == 'bottom_left':
@@ -59,6 +64,9 @@ def get_possible_positions(width, height, all_selected_zones, all_tags_shapes):
     if best_solution:
         for i in range(len(all_selected_zones)):
             info_zone = best_solution['zone_{}'.format(i)]
+            # This is for testing where this method thinks tags will be poisitionated
+            if len(frame):
+                cv2.rectangle(frame, (info_zone[0],info_zone[1]),(info_zone[2], info_zone[3]), (0,255,0),0)
             if info_zone[0] >= 0 and info_zone[2] <= width and info_zone[1] >= 0 and info_zone[3] <= height:
                 final_ans.append(info_zone[4])
             else:
@@ -71,4 +79,4 @@ if __name__ == '__main__':
     height = 720
     all_selected_zones = [(100,100,200,300), (220,100,400,300)]
     all_tags_shapes = [(70,80), (70,80)]
-    pprint.pprint(get_possible_positions(width, height, all_selected_zones, all_tags_shapes))
+    print(get_possible_positions(width, height, all_selected_zones, all_tags_shapes))
