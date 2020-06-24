9fb55f1762e3b7d0e94029fbfa9319d491de5848
Fernando <fernaperg@gmail.com>
2020-06-24 20:55:33 +0100

    :tada: Updated version, now we don't need anything about constraint programming (efficiency improvement)

--- cv2_tools/__init__.py
+++ cv2_tools/__init__.py
@@ -6,7 +6,7 @@ MIT License
 Copyright (c) 2019 Fernando Perez
 For more information visit: https://github.com/fernaper/cv2-tools
 Also you can write complete_help to view full information'''
-__version__ = '2.3.5'
+__version__ = '2.4.0'
 
 complete_help = '''
 {} - v{}
